a6833d5ac1a80e004298ea60f19c6da4e71c1b36
Ignas Davulis <ignas.davulis@swedbank.lt>
2024-04-26 16:01:39 +0300

    Initial Commit

---
+++ RickNMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickNMorty
+//
+//  Created by Ignas Davulis on 26/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
